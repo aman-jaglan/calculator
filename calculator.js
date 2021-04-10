f0a80341df6bbd0c13406048848291d621c31448
aman-jaglan <amanjaglan1819@gmail.com>
2021-04-10 09:13:41 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,21 @@
+//jshint esversion:6
+const express= require("express");
+const bodyParser=require("body-parser");
+const app=express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/",function(req,res){
+
+  res.sendFile(__dirname+ "/index.html");
+});
+app.post("/",function(req,res){
+
+  var n1= Number(req.body.num1);
+  var n2= Number(req.body.num2);
+  var ans= n1+n2;
+  res.send("the result of calculation is"+ans);
+});
+app.listen(3000,function(){
+
+  console.log("server is working on 3000");
+});
